08e9e7a23525d7c871698220eaaf4c02530249de
Miguel Bustamante <miguel10bustamante21@gmail.com>
2023-07-19 21:40:12 -0600

    Day6: Open and read files with python

---
+++ udemyCourse/day6/open-read-files/open-manage-files.py
@@ -0,0 +1,18 @@
+"""
+Date: 18/10/2023
+Miguel Ángel Bustamante Pérez
+
+* Open and manipulate files with Python
+
+TODO: This program is only for educational purposes, in this case to learn how to open and manage files using Python methods.
+
+Comments:
+
+"""
+
+my_file = open("Prueba.txt", encoding="latin-1")
+
+print(my_file.read())
+
+my_file.close()
+
