ed16324f369d489a25a52b5147b3b45f966934e3
Miguel Bustamante <miguel10bustamante21@gmail.com>
2023-07-19 22:09:59 -0600

    Day6: Add create and write python files

---
+++ udemyCourse/day6/create-write-files/create-write-files.py
@@ -0,0 +1,18 @@
+"""
+Date: 19/10/2023
+Miguel Ángel Bustamante Pérez
+
+* Create and write files
+
+TODO: This program is only for educational purposes, in this case to learn how to write and create files using Python methods.
+
+Comments:
+
+"""
+
+# * Read only method
+file = open("../open-read-files/Prueba.txt", "w")
+
+file.write("I'm the new text")
+
+file.close()
