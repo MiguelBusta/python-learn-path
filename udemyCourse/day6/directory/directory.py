26de026110768f83da7e06d3ed7db890ed79067c
Miguel Bustamante <miguel10bustamante21@gmail.com>
2023-08-02 07:07:11 -0600

    Day6: Add files to directory folder

---
+++ udemyCourse/day6/directory/directory.py
@@ -0,0 +1,11 @@
+"""
+Date: 08/01/2023
+Miguel Ángel Bustamante Pérez
+
+* Directories
+
+TODO: This program is only for educational purposes, in this case to learn how to use methods to manipulate routes and directories.
+
+Comments:
+
+"""
