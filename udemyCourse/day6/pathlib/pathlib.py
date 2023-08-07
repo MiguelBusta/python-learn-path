83eb942ef6aecaf36638fa270bd3391793e25849
Miguel Bustamante <miguel10bustamante21@gmail.com>
2023-08-07 09:56:17 -0600

    Day6: pathlib

---
+++ udemyCourse/day6/pathlib/pathlib.py
@@ -0,0 +1,14 @@
+"""
+Date: 08/07/2023
+Miguel Ángel Bustamante Pérez
+
+* Directories
+
+TODO: This program is only for educational purposes, in this case to learn how to use the pathlib module
+
+Comments:
+
+"""
+
+from pathlib import Path
+
