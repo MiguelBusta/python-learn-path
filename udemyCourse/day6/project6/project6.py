73b32b619fba7ef84ecadf48dbeb6eb24aa5e2f5
Miguel Bustamante <miguel10bustamante21@gmail.com>
2024-03-05 11:36:17 -0600

    Add more changes to project 6

---
+++ udemyCourse/day6/project6/project6.py
@@ -0,0 +1,13 @@
+"""
+Date: 02/09/2024
+Miguel Ángel Bustamante Pérez
+
+* Project 6
+
+TODO: This program is only for educational purposes, in this case it's a project about managing files in folders.
+
+Comments:
+
+"""
+
+from pathlib import Path
